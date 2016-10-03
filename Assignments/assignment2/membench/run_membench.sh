339de881a802ac8dea62f0264f0a2af8f1bf388a
Radim Janalík <radim.janalik@gmail.com>
2016-10-04 00:48:13 +0200

    Fixed problem with run_membench.sh

--- Assignments/assignment2/membench/run_membench.sh
+++ Assignments/assignment2/membench/run_membench.sh
@@ -9,4 +9,4 @@
 # load modules
 module load gcc/6.1.0 gnuplot
 
-./membench | sed -e '/:/	s//: /g' -e '/  */	s//	/g' | cut -f2,4,6 > generic.xxx && gnuplot generic.gp
+./membench | sed -e '/:/	s//: /g' -e '/  */	s//	/g' | cut -f2,4,6 > generic.xxx && sed -e '/sarlacc/ s//generic/' gnuplot.template > generic.gp && gnuplot generic.gp
