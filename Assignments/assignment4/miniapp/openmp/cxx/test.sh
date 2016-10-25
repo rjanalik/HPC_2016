72afcc473b65fe673e68c0dacae5b6664c62ef58
Radim Janalík <radim.janalik@gmail.com>
2016-10-25 12:14:38 +0200

    Modified test.sh in assignment4/miniapp

--- Assignments/assignment4/miniapp/openmp/cxx/test.sh
+++ Assignments/assignment4/miniapp/openmp/cxx/test.sh
@@ -1,7 +1,7 @@
 for dim in 64 128 256 512 1024
 do
     echo ======= ${dim}x${dim}
-    for t in 1 2 4 8
+    for t in {1..10}
     do
         printf "%3d threads : "  $t
         OMP_NUM_THREADS=$t srun -n1 -c10 --hint=nomultithread ./main $dim $dim 100 0.01 | grep took;
